711a350c02303bad5fbd0625572ef9b76bc6502e
Abhinav Singh <71094720+abhinav0321@users.noreply.github.com>
2022-09-13 10:09:43 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,10 @@
+class Sample {
+    int x = 10, y = 15;
+
+    public static void main(String a[]) {
+        Sample s = new Sample();
+        System.out.print("value of x:- " + s.x + " " + s.y);
+
+    }
+
+}
