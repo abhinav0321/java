552de72b7765bf531ce705be4383ba8ca30f39a0
Abhinav Singh <71094720+abhinav0321@users.noreply.github.com>
2022-09-13 10:46:18 +0530

    Area of Trianlge. Values are declared.

---
+++ standard programs/Areaoftrianlge.java
@@ -0,0 +1,9 @@
+class Areaoftriangle {
+
+    public static void main(String a[]) {
+        int height = 10, base = 10;
+        System.out.println("Area is " + (height * base) * .5);
+
+    }
+
+}
