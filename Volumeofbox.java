3109b3c2e80a193655b02821f7dae80250b281e0
Abhinav Singh <71094720+abhinav0321@users.noreply.github.com>
2022-09-13 10:29:35 +0530

    Add files via upload

---
+++ Volumeofbox.java
@@ -0,0 +1,10 @@
+class Volumeofbox {
+    int length = 5, breadth = 10, height = 2;
+
+    public static void main(String a[]) {
+        Volumeofbox obj = new Volumeofbox();
+        System.out.println("Volume is " + obj.length * obj.breadth * obj.height);
+
+    }
+
+}
